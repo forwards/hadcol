e8789453be5bfd0b24cf6c603856a6239f675071
Heather Turner <ht@heatherturner.net>
2018-06-23 08:16:36 +0100

    simplify to add_cols only

--- tests/testthat/test-add-cols.R
+++ tests/testthat/test-add-cols.R
@@ -1,30 +1,15 @@
 context("add_cols")
 
-test_that("positive where arguments place RHS correctly", {
-  df1 <- data.frame(a = 3, b = 4, c = 5)
-  df2 <- data.frame(X = 1, Y = 2)
-  at_pos <- function(where) {
-    add_cols(df1, df2, where = where)
-  }
+df1 <- data.frame(a = 3, b = 4, c = 5)
+df2 <- data.frame(X = 1, Y = 2)
 
+at_pos <- function(where) {
+  add_cols(df1, df2, where = where)
+}
+
+test_that("can add column at any position", {
   expect_named(at_pos(1), c("X", "Y", "a", "b", "c"))
   expect_named(at_pos(2), c("a", "X", "Y", "b", "c"))
   expect_named(at_pos(3), c("a", "b", "X", "Y", "c"))
   expect_named(at_pos(4), c("a", "b", "c", "X", "Y"))
-})
-
-
-# where -------------------------------------------------------------------
-
-test_that("where must be valid value", {
-  expect_error(check_where("a"), "length one numeric vector")
-  expect_error(check_where(1:10), "length one numeric vector")
-
-  expect_error(check_where(0), "not be zero or missing")
-  expect_error(check_where(NA_real_), "not be zero or missing")
-})
-
-test_that("where correctly transforms negative values", {
-  expect_equal(check_where(-1, 3), 4)
-  expect_equal(check_where(-4, 3), 1)
 })
